4426f01a893fce260e7d26bc5bfe68498736e8ae
Seun Adekunle <adekunleseun001@gmail.com>
2025-08-16 16:18:24 +0100

    add tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000
-  },
   resolve:{
     alias: {
       '@': '/src'
